df435c3efef6c72c6d136f7f008b7039c50d5b7a
vkatsel <vkatsel@kse.org.ua>
2024-11-27 01:22:41 +0200

    added drawing graphs

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+
+a = float(input('A: '))
+b = float(input('B: '))
+c = float(input('C: '))
+
+domain = np.arange(-10, 10)
+
+range_quadrilateral = a * (domain ** 2) + b * domain + c
+range_logarithmic = np.log(domain) / np.log(2)
+
+mpl.plot(domain, range_quadrilateral, marker='o')
+mpl.plot(domain, range_logarithmic, marker='o')
+mpl.show()
+
+
+
+
